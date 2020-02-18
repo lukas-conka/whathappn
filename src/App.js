a5f1ea7314aa1231bd00ddcda874b8c7d6ba191f
Lucas Santos <lucas.santos@pcservice.com.br>
2020-02-18 18:07:06 -0300

    firts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
-function App() {
+export default function App() {
+  
+  const [repositories, setRepositories] = useState([
+    {id: 1, name: 'repo-teste-1'},
+    {id: 2, name: 'repo-teste-2'},
+    {id: 3, name: 'repo-teste-3'}
+  ])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {repositories.map(repo => <li key={repo.id}>{repo.name}</li>)}
+    </ul>
+  )
 }
-
-export default App;
